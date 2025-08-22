0a425d820f282246d1830289271b665a406b3507
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2025-08-22 15:49:08 +0100

    go

---
+++ src/data/publications.js
@@ -0,0 +1,124 @@
+export const PublicationsList = [
+  {
+    id: '1',
+    title:
+      'Review of the ecology and behaviour of Aedes aegypti and Aedes albopictus in Western Africa and implications for vector control',
+    authors:
+      'Beatrice R Egid, Mamadou Coulibaly, Samuel Kweku Dadzie, Basile Kamgang, Philip J McCall, Luigi Sedda, Kobié Hyacinthe Toe, Anne L Wilson',
+    year: '2022',
+    journal: 'Current Research in Parasitology & Vector-Borne Diseases',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '2',
+    title:
+      'Mosquito-borne disease, waste pickers and health equity: a co-produced study in Andhra Pradesh, India',
+    authors: 'Beatrice Egid',
+    year: '2024',
+    journal: 'Liverpool School of Tropical Medicine (PhD Thesis)',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '3',
+    title:
+      'Can everyone hear me? Reflections on the use of global online workshops for promoting inclusive knowledge generation',
+    authors:
+      'Beatrice Egid, Kim Ozano, Guillermo Hegel, Emily Zimmerman, Yaimie López, Maria Roura, Payam Sheikhattari, Laundette Jones, Sónia Dias, Nina Wallerstein',
+    year: '2023',
+    journal: 'Qualitative Research',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '4',
+    title:
+      '‘You want to deal with power while riding on power’: global perspectives on power in participatory health research and co-production approaches',
+    authors:
+      'Beatrice R Egid, María Roura, Bachera Aktar, Jessica Amegee Quach, Ivy Chumo, Sónia Dias, Guillermo Hegel, Laundette Jones, Robinson Karuga, Luret Lar, ...',
+    year: '2021',
+    journal: 'BMJ Global Health',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '5',
+    title:
+      'Integrating city resilience and mosquito-borne diseases--a multi-site case study from the Resilient Cities Network',
+    authors:
+      'Beatrice R Egid, Marcela Herrera Jaramillo, Thomas C Lindsay, Clara Isabel Lopez Villegas, Krishna Mohan, Kim Ozano, Raul Alberto Rojo Ospina, Carlos Alberto Sarria Ocampo, Bindu Taylor-Brewer, Carlos Andrés Villarreal Restrepo, ...',
+    year: '2023',
+    journal: 'Cities & Health',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '6',
+    title:
+      'You want to deal with power while riding on power”: global perspectives on power in participatory health research and co-production approaches. BMJ Glob Health 6 (11): e006978',
+    authors:
+      'BR Egid, M Roura, B Aktar, J Amegee Quach, I Chumo, S Dias, G Hegel, L Jones, R Karuga, L Lar, ...',
+    year: '2021',
+    journal: null,
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '7',
+    title: 'Health Equity and Rights',
+    authors: 'Beatrice Egid',
+    year: '2024',
+    journal:
+      'The Institute of Development Studies and Partner Organisations',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '8',
+    title:
+      'Six guiding principles to support equity in global health programmes',
+    authors: 'Beatrice Egid, Kerry Millington, Sally Theobald, Sushil Baral',
+    year: '2025',
+    journal:
+      'The Institute of Development Studies and Partner Organisations',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '9',
+    title: 'Health Equity Fact Sheet',
+    authors: 'Beatrice Egid, Kerry Millington',
+    year: '2025',
+    journal:
+      'The Institute of Development Studies and Partner Organisations',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '10',
+    title:
+      'Health Equity and Rights: Research Case Study-Equity, Health and Wellbeing of People Living and Working in Informal Urban Settlements',
+    authors: 'Sabina Rashid, Lilian Otiso, Kerry Millington, Beatrice Egid',
+    year: '2025',
+    journal:
+      'The Institute of Development Studies and Partner Organisations',
+    journalLogo: null,
+    doi: null,
+  },
+  {
+    id: '11',
+    title:
+      'Antimicrobial resistance, equity and justice in low-and middle-income countries: an intersectional critical interpretive synthesis',
+    authors:
+      'Katy Davis, Ralalicia Limato, Meenakshi Monga, Beatrice Egid, Sneha Paul, Susan Okioma, Owen Nyamwanza, Abriti Arjyal, Syeda Tahmina Ahmed, Ayuska Parajuli, ...',
+    year: '2025',
+    journal: 'Nature Communications',
+    journalLogo: null,
+    doi: null,
+  },
+];
+
+
+export const PostersList = [
+];
